50f58587fa2df74b0725ef65534d035c01d7b6e4
Mayank Gupta <you@example.com>
2023-02-27 13:25:25 +0530

    commit

--- src/data.js
+++ src/data.js
@@ -1,16 +1,14 @@
 const list = [
   {
     id: 1,
-    title: "My First Book of Pencil Control",
-    author: "by Wonder House Books | 25 April 2018",
+    title: "Canon EOS 1500D 24.1 Digital SLR Camera (Black) with EF S18-55 is II Lens",
     price: 2999,
     img: "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
     amount: 1,
   },
   {
     id: 2,
-    title: "108 Panchatantra Stories",
-    author: "by Maple Press  | 1 September 2020",
+    title: "Men's Mesh Lace-up Sports Shoes",
     price: 599,
     img: "https://petapixel.com/assets/uploads/2017/03/product1.jpeg",
     amount: 1,
@@ -18,7 +16,6 @@ const list = [
   {
     id: 3,
     title: "Hemt Analog Black Dial Men's Watch-HM-GR092-BLK-CH      ",
-    author: "by Om Books Editorial Team  | 25 November 2018",
     price: 1599,
     img: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80",
     amount: 1,
@@ -26,7 +23,6 @@ const list = [
   {
     id: 4,
     title: "Cotton Plain V Neck T-shirt",
-    author: "by Wonder House Books | 25 April 2018",
     price: 699,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs5PBmd2kb2pYcgV6NBDTIT_f1YJpguqDUGgnHRRtTIgUjDdsR6jwfrkv9-9xnDWCIuFg&usqp=CAU",
     amount: 1,
@@ -34,7 +30,6 @@ const list = [
   {
     id: 5,
     title: "Apple iPhone 14 128GB (Product) RED",
-    author: "by Wonder House Books  | 1 January 2018",
     price: 78999,
     img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-finish-select-202207-6-1inch-product-red_AV1_GEO_EMEA_FMT_WHH?wid=1280&hei=492&fmt=p-jpg&qlt=80&.v=1656720500988",
     amount: 1,
@@ -42,15 +37,13 @@ const list = [
   {
     id: 6,
     title: "Transparent Square Unisex Optical Frame (2379W)",
-    author: "by Om Books Editorial Team | 30 September 2020",
     price: 1399,
     img: "https://cdn.pixabay.com/photo/2020/05/26/09/32/product-5222398_960_720.jpg",
     amount: 1,
   },
   {
     id: 7,
-    title: "Pre-School Activities Pack ",
-    author: "by Om Books Editorial Team  | 1 January 2021",
+    title: "Cello Venice Plastic Bottle Set, 1 Liter ",
     price: 1199,
     img: "https://images.unsplash.com/photo-1602143407151-7111542de6e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Ym90dGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
     amount: 1,
@@ -58,7 +51,6 @@ const list = [
   {
     id: 8,
     title: "Men Jeans Pant Light Blue Denim Regular Fit | Estilo Fashions",
-    author: "by Wonder House Books  | 6 December 2020",
     price: 999,
     img: "https://estilofashions.com/wp-content/uploads/2021/02/Mens-Jeans-Pant-2-3.jpg",
     amount: 1,
@@ -66,7 +58,6 @@ const list = [
   {
     id: 9,
     title: "Kitchen Tools Pictures",
-    author: "by Content Team at Target Publications",
     price: 1899,
     img: "https://media.istockphoto.com/id/1337429990/photo/pattern-made-from-cooking-utensil-set-silicone-kitchen-tools-with-wooden-handle-on-beige.jpg?b=1&s=170667a&w=0&k=20&c=d3qDAKXTHEml9YNwvVdIYLEtfxzKkFqJIfKJp2s_So0=",
     amount: 1,
@@ -74,7 +65,6 @@ const list = [
   {
     id: 10,
     title: "Lavie Sport Chairman Laptop Bag | ",
-    author: "by Maple Press | 1 September 2021",
     price: 1299,
     img: "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFnfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
     amount: 1,
@@ -82,7 +72,6 @@ const list = [
   {
     id: 11,
     title: "Philips BT1232/15 Skin-Friendly Beard Trimmer - DuraPower",
-    author: "by Rebecca Smith  | 14 December 2021",
     price: 1799,
     img: "https://images.unsplash.com/photo-1625772452888-ca36edeaf3a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dHJpbW1lcnxlbnwwfHwwfHw%3D&w=1000&q=80",
     amount: 1,
@@ -90,7 +79,6 @@ const list = [
   {
     id: 12,
     title: "X 12th Gen Intel Evo Core i7 13.3 inch",
-    author: "by Sudha Murty | 1 January 2015",
     price: 157,
     img: "https://images.unsplash.com/photo-1618424181497-157f25b6ddd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wJTIwY29tcHV0ZXJ8ZW58MHx8MHx8&w=1000&q=80",
     amount: 1,
